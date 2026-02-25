16adc359e35afe67ba71903efc13e2813bfc7733
LOtux_VO <edgar.pulles@gmail.com>
2026-02-25 14:18:16 +0100

    exo7 -petit 1

--- exo7.php
+++ exo7.php
@@ -9,11 +9,11 @@
  <h1>Exercices - Les passages de données</h1>
  <h2>Exercice 1 :</h2>
  <p>
-     1) Créer un fichier <code>resultats.php</code> dans le dossier exo (au même niveau que les fichiers exo1.php, exo2.php,...). <br>
+     1) Créer un fichier <code>exo7_resultats.php</code> dans le dossier exo (au même niveau que les fichiers exo1.php, exo2.php,...). <br>
      2) Méthode GET : Ici dans le fichier exo7.php, ajouter un lien comme dans le cours du chapitre 7 avec comme paramètre dans l'URL votre nom et votre prenom<br>
-     Puis faire afficher le résultat dans la page <code>resultats.php</code><br>
+     Puis faire afficher le résultat dans la page <code>exo7_resultats.php</code><br>
      3) Méthode POST : Puis toujours ici, créer un formulaire pour saisir le nom et prénom comme dans le cours<br>
-     Puis faire afficher le résultat dans la page <code>resultats.php</code><br>
+     Puis faire afficher le résultat dans la page <code>exo7_resultats.php</code><br>
      Tester avec plusieurs valeurs :)
  </p>
 
@@ -21,9 +21,16 @@
  <br>
 
  <?php
-    // Code Here
+    echo '<a href="exo7_resultats.php?nom=Pulles&prenom=Edgar">Lien avec parametres en GET</a>';
+
     ?>
 
+    <form action="exo7_resultats.php" method="post">
+         Prénom : <input type="text" name="prenom" />
+         Nom : <input type="text" name="nom" />
+
+        <input type="submit" value="valider"/>
+    </form>
 
 
  <h2>Exercice 2 :</h2>
@@ -32,7 +39,7 @@
      Créer tout d'abord les champs de saisi pour l'âge, attention quand on récupérera sa valeur car si on saisi 16, on va en réalité récupérer "16" et non 16. Pour transformer cela en entier, il faut utiliser la fonction <a href="http://php.net/manual/fr/function.intval.php">intval</a><br>
      Puis créer des radio boutons pour le sexe, attention la valeur retournée sera celle inscrite dans la <code>value</code> de l'<code>input</code> <br><br>
      Continuer le formulaire de l'exercice 1 et récupérer les résultats dans le fichier <code>
-         resultats.php</code>. Tester votre code par rapport aux conditions de l'<strong>exercice 3 du chapitre 3</strong> !
+         exo7_resultats.php</code>. Tester votre code par rapport aux conditions de l'<strong>exercice 3 du chapitre 3</strong> !
  </p>
 
 
---
+++ exo7_resultats.php
@@ -0,0 +1,5 @@
+<?php
+ echo "en GET : nom : " . $_GET['nom'] . ", prenom : " . $_GET['prenom']."<br>";
+ 
+ echo "en POST : nom : " . $_POST['nom'] . ", prenom : " . $_POST['prenom']."<br>";
+?>
